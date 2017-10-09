aa8f467f79781411d36608141ffce25048709345
Alastair Christian <aj.christian@datadigest.net>
2017-10-09 16:44:48 +0200

    #3101 - create basic API implementation

---
+++ PinotageTodo.Web/Controllers/TodosController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using PinotageTodo.Web.Models;
+
+namespace PinotageTodo.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodosController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<TodoApiModel> GetAll()
+        {
+            return new List<TodoApiModel>()
+            {
+                new TodoApiModel() { Id = Guid.NewGuid(), Name = "Todo 1", IsCompleted = false },
+                new TodoApiModel() { Id = Guid.NewGuid(), Name = "Todo 2", IsCompleted = true }
+            };
+        }
+
+		[HttpPost("add", Name = "AddTodo")]
+		public IActionResult Add([FromBody] TodoApiModel item)
+		{
+			if (item == null)
+			{
+				return BadRequest();
+			}
+
+			return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+		}
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult Get(Guid id)
+        {
+            var model = new TodoApiModel() { Id = id, Name = "I'm a stub", IsCompleted = false };
+            return new ObjectResult(model);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, [FromBody] TodoApiModel item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            return new NoContentResult();
+        }
+    }
+}
---
+++ PinotageTodo.Web/Models/TodoApiModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PinotageTodo.Web.Models
+{
+    public class TodoApiModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
