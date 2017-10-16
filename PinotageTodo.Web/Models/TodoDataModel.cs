f7e56d7e04ecc0eb29b7557a3743d73864b4c8ed
Alastair Christian <aj.christian@datadigest.net>
2017-10-16 10:30:05 +0200

    #3102 - Added ToDoRepository interface

---
+++ PinotageTodo.Web/Models/TodoDataModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PinotageTodo.Web.Models
+{
+    public class TodoDataModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ PinotageTodo.Web/Repository/ITodoRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using PinotageTodo.Web.Models;
+
+namespace PinotageTodo.Web.Repository
+{
+    public interface ITodoRepository
+    {
+        void Add(TodoDataModel model);
+
+        void Update(TodoDataModel model);
+
+        void Delete(Guid id, Guid userId);
+
+        IEnumerable<TodoDataModel> GetAll(Guid userId);
+    }
+}
